856a59d43dfabc91b7b8a9b593c8b480c3b4b701
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-02-19 17:22:58 +0400

    fixup! [SLC] Support Kotlin collection classes mapped to Java classes
    Update testdata after introducing FULL_JDK directive:
    
    - it revealed that we generate stubs for static methods - it's a bug
    - 'remove' methods disappeared from read-only Iterator because they are default,
    and we don't generate stubs for default methods - this is correct

--- compiler/testData/asJava/lightClasses/lightClassByPsi/k2/collections/mutable/mutableMapEntryClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/k2/collections/mutable/mutableMapEntryClass.java
@@ -1,4 +1,16 @@
 public abstract class CMutableMapEntry /* test.CMutableMapEntry*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
+
   public  CMutableMapEntry();//  .ctor()
 }
 
@@ -13,6 +25,18 @@ public abstract class CMutableMapEntry2 /* test.CMutableMapEntry2*/<KElem, VElem
   @java.lang.Override()
   public VElem getValue();//  getValue()
 
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
+
   public  CMutableMapEntry2();//  .ctor()
 }
 
@@ -25,6 +49,18 @@ public class CMutableMapEntry3 /* test.CMutableMapEntry3*/<KElem, VElem>  implem
 
   @java.lang.Override()
   public VElem setValue(VElem);//  setValue(VElem)
+
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
 
   public  CMutableMapEntry3();//  .ctor()
 }
--- compiler/testData/asJava/lightClasses/lightClassByPsi/k2/collections/mutable/mutableMapEntryInterfaceOfString.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/k2/collections/mutable/mutableMapEntryInterfaceOfString.java
@@ -1,4 +1,16 @@
 public abstract class CMutableMapEntry /* test.CMutableMapEntry*/ implements test.IMutableMapEntry {
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
+
   public  CMutableMapEntry();//  .ctor()
 }
 
@@ -16,6 +28,18 @@ public abstract class CMutableMapEntry2 /* test.CMutableMapEntry2*/ implements t
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.lang.String getValue();//  getValue()
 
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
+
   public  CMutableMapEntry2(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() test.IMutableMapEntry);//  .ctor(@org.jetbrains.annotations.NotNull() test.IMutableMapEntry)
 }
 
@@ -31,6 +55,18 @@ public class CMutableMapEntry3 /* test.CMutableMapEntry3*/ implements test.IMuta
   @java.lang.Override()
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.lang.String setValue(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  setValue(@org.jetbrains.annotations.NotNull() java.lang.String)
+
+  @java.lang.Override()
+  public static <K extends java.lang.Comparable<? super K>, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey();// <K extends java.lang.Comparable<? super K>, V>  comparingByKey()
+
+  @java.lang.Override()
+  public static <K, V extends java.lang.Comparable<? super V>> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue();// <K, V extends java.lang.Comparable<? super V>>  comparingByValue()
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByKey(java.util.Comparator<? super K>);// <K, V>  comparingByKey(java.util.Comparator<? super K>)
+
+  @java.lang.Override()
+  public static <K, V> java.util.Comparator<java.util.Map.Entry<K, V>> comparingByValue(java.util.Comparator<? super V>);// <K, V>  comparingByValue(java.util.Comparator<? super V>)
 
   public  CMutableMapEntry3();//  .ctor()
 }
