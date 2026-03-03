12bde4e0c0b1ddbac0e32a4d8fca7cc73357dd38
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-03-03 13:05:41 +0000

    fixup! [K/JS] Allow exporting value classes

--- compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsExportDeclarationChecker.kt
+++ compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsExportDeclarationChecker.kt
@@ -168,7 +168,7 @@ object FirJsExportDeclarationChecker : FirBasicDeclarationChecker(MppCheckerKind
                     ClassKind.ANNOTATION_CLASS -> "annotation class"
                     ClassKind.CLASS -> when {
                         !context.languageVersionSettings.supportsFeature(LanguageFeature.AllowInterfaceNestedClassesInJsExport) && context.isInsideInterface -> "nested class inside exported interface"
-                        !LanguageFeature.JsAllowExportingValueClasses.isEnabled() && declaration.isInlineOrValue -> "value class"
+                        LanguageFeature.JsAllowExportingValueClasses.isDisabled() && declaration.isInlineOrValue -> "value class"
                         else -> null
                     }
 
