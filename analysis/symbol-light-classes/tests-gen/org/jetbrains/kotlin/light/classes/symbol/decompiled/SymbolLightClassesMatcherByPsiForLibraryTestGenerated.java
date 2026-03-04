ce34ff56581ce9aa9741c61d5d46b95474371785
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-03-04 19:07:37 +0400

    customAnnotation test

--- analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/decompiled/SymbolLightClassesMatcherByPsiForLibraryTestGenerated.java
+++ analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/decompiled/SymbolLightClassesMatcherByPsiForLibraryTestGenerated.java
@@ -609,6 +609,12 @@ public class SymbolLightClassesMatcherByPsiForLibraryTestGenerated extends Abstr
       }
 
       @Test
+      @TestMetadata("customAnnotation.kt")
+      public void testCustomAnnotation() {
+        run("customAnnotation.kt");
+      }
+
+      @Test
       @TestMetadata("explicitOverrides.kt")
       public void testExplicitOverrides() {
         run("explicitOverrides.kt");
--- analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/decompiled/SymbolLightClassesParentingByPsiForLibraryTestGenerated.java
+++ analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/decompiled/SymbolLightClassesParentingByPsiForLibraryTestGenerated.java
@@ -609,6 +609,12 @@ public class SymbolLightClassesParentingByPsiForLibraryTestGenerated extends Abs
       }
 
       @Test
+      @TestMetadata("customAnnotation.kt")
+      public void testCustomAnnotation() {
+        run("customAnnotation.kt");
+      }
+
+      @Test
       @TestMetadata("explicitOverrides.kt")
       public void testExplicitOverrides() {
         run("explicitOverrides.kt");
