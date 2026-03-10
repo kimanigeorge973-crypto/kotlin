9c26c8611f6678b161f4c1beab093c0ec07e39fb
Florian Freitag <florian.freitag.external@jetbrains.com>
2026-03-10 07:59:57 +0100

    [FIR] Store unsigned values as signed long values
    Later stages of the compiler expect signed values and won't behave if the
     constant expression is unsigned.
     ^KT-84627

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/expressions/FirExpressionEvaluator.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/expressions/FirExpressionEvaluator.kt
@@ -659,10 +659,19 @@ private fun Any?.toConstExpression(
     kind: ConstantValueKind,
     originalExpression: FirExpression
 ): FirLiteralExpression {
+    // Later stages of the compiler expect signed values
+    val value = when (this) {
+        is UByte -> this.toLong()
+        is UShort -> this.toLong()
+        is UInt -> this.toLong()
+        is ULong -> this.toLong()
+        else -> this
+    }
+
     return buildLiteralExpression(
         originalExpression.source,
         kind,
-        this,
+        value,
         originalExpression.annotations.takeIf { it.isNotEmpty() }?.toMutableList(),
         setType = false,
     ).apply { replaceConeTypeOrNull(originalExpression.resolvedType) }
